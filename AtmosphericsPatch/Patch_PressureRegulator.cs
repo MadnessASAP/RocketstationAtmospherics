2f308dfd7b7a506d6f3fc7b892448a0ba36799cf
Michael Weinrich <michael@a5ap.net>
2018-08-05 10:07:51 -0700

    Branched the NetworkServer:Spawn patch, it doesn't work but it may be handy in the future

--- AtmosphericsPatch/Patch_PressureRegulator.cs
+++ AtmosphericsPatch/Patch_PressureRegulator.cs
@@ -1,39 +1,11 @@
-﻿using System.Reflection.Emit;
-using System.Collections.Generic;
+﻿using System;
+using System.Reflection;
+using UnityEngine;
 using Harmony;
 
 namespace AtmosphericsPatch
 {
-    [HarmonyPatch(typeof(Assets.Scripts.Objects.Pipes.PressureRegulator), "OnAtmosphericTick")]
-    class Patch_PressureRegulator_OnAtmosphericTick
-    {
-        [HarmonyTranspiler]
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            yield return new CodeInstruction(OpCodes.Ldarg_0);
-            yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Assets.Scripts.Objects.Pipes.Device), "OnAtmosphericTick"));       // call base.OnAtmosphericTick
-            yield return new CodeInstruction(OpCodes.Ldarg_0);
-            yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Patch_PressureRegulator_OnAtmosphericTick), "_newOnAtmosphericTick")); // call our OnAtmosphericTick method
-            yield return new CodeInstruction(OpCodes.Ret);
-        }
 
-        private static void _newOnAtmosphericTick(Assets.Scripts.Objects.Pipes.PressureRegulator device)
-        {
-            if (!device.OnOff || !device.Powered || device.Error == 1 ||
-                device.InputNetwork == null || device.OutputNetwork == null) return;
-            var sim = new RocketstationAtmospherics.PressureRegulator(device.RegulatorType,
-                    inputAtmosphere: device.InputNetwork.Atmosphere,
-                    outputAtmosphere: device.OutputNetwork.Atmosphere)
-            {
-                PressureLimit = (float)device.Setting * 1000
-            };
-            sim.Tick();
-        }
-    }
-
-    // This was a really not great way to do things that involved extending the PressureRegulator class and injecting it into GameObjects are they spawned.
-    // It failed for many reasons but I'll leave it here as a tombstone that may be useful down the road.
-#if false
     [HarmonyPatch(typeof(UnityEngine.Networking.NetworkServer), "Spawn", new Type[] { typeof(GameObject) })]
     public class Patch_NetworkServer_Spawn
     {
@@ -98,6 +70,5 @@ namespace AtmosphericsPatch
             RocketstationAtmospherics.PressureRegulator pr;
         }
         
-    }  
-#endif
+    }
 }
