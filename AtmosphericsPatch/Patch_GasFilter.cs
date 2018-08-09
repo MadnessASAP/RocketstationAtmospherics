0cf19b3c88f9ec5430689fbba15dcc849d999d03
Michael Weinrich <michael@a5ap.net>
2018-08-08 18:40:48 -0700

    A helping hand for a sandwich in need, also makes GasFilters never deplete.

---
+++ AtmosphericsPatch/Patch_GasFilter.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using Assets.Scripts.Objects.Items;
+
+namespace AtmosphericsPatch
+{
+    [HarmonyPatch(typeof(GasFilter))]
+    [HarmonyPatch("CheckUsedTicks")]
+    class Patch_GasFilter_CheckUsedTicks
+    {
+        static void Postfix(ref int ____usedTicks)
+        {
+            ____usedTicks = 0;
+        }
+    }
+}
